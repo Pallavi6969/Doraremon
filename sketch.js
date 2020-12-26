f0bd284c28165efb922035afc8633d2231045812
Pallavi6969 <74306328+Pallavi6969@users.noreply.github.com>
2020-12-26 13:22:58 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var doraemon,doraemon_running,doraemon_lose,doraemon_won;
+var bg,bg_image;
+var ground;
+function preload(){
+doraemon_running=loadAnimation("Image\clipart4224351.png")
+bg_image=loadImage("Image\playground.png");
+}
+function setup(){createCanvas(1000,600);
+    bg=createSprite(500,300,1000,600);
+    bg.addImage(bg_image);
+    bg.scale=0.5;
+
+    doraemon=createSprite(200,400,20,50);
+    doraemon.addImage(doraemon_running);
+    ground=createSprite(200,585,400,10);
+    ground.visible=false;
+    
+}
+function draw(){
+    drawSprites()
+    if(keyDown("space")){
+        doraemon.velocityY=-16;
+        doraemon.velocityY=doraemon.velocityY+0.5;
+    }
+    doraemon.collide(ground)
+    doraemon.debug=true;
+
+}
